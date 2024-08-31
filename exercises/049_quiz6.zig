c382c2eac20f48551602851aac58c00a5793927d
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2024-08-31 18:43:04 -0400

    exercises 45 - 50

--- exercises/049_quiz6.zig
+++ exercises/049_quiz6.zig
@@ -25,11 +25,13 @@ const Elephant = struct {
     }
 
     // Your Elephant trunk methods go here!
-    // ---------------------------------------------------
+    pub fn getTrunk(self: *Elephant) *Elephant {
+        return self.trunk.?;
+    }
 
-    ???
-
-    // ---------------------------------------------------
+    pub fn hasTrunk(self: *Elephant) bool {
+        return (self.trunk != null);
+    }
 
     pub fn visit(self: *Elephant) void {
         self.visited = true;
